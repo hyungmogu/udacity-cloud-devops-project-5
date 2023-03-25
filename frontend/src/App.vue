62d0c10169bf27c58a7775dba521b9cc55f2fd05
Hyungmo Gu <guhyungm7@gmail.com>
2023-03-25 16:10:10 -0600

    feat: Creating frontend #604

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from 'vue-router';
+import ITCFooter from './components/ITCFooter.vue';
 </script>
 
 <template>
   <RouterView />
+  <ITCFooter/>
 </template>
 
 <style scoped>
---
+++ frontend/src/components/ITCFooter.vue
@@ -0,0 +1,21 @@
+<template>
+    <footer class="itc-footer">
+        <div class="container">
+            <div class="itc-footer__logo">
+                 Hyungmo Gu
+            </div>
+            <div class="itc-footer__sns">
+
+            </div>
+        </div>
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+.itc-footer {
+    padding: 60px 0;
+    background: var(--primary);
+    color: var(--white);
+}
+
+</style>
