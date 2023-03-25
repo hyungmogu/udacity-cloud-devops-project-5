aab41b2dda29b78da68876d57d863349b6942da1
Hyungmo Gu <guhyungm7@gmail.com>
2023-03-24 20:57:33 -0600

    feat: Creating frontend #131

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -5,23 +5,39 @@ import ITCConvertTool from '../components/ITCConvertTool.vue'
 </script>
 
 <template>
-  <main>
+  <main class="page">
     <div class="container">
-      <aside class="sidebar-advertisement sidebar-advertisement--left">
-        <ITCAdvertisement direction="vertical"/>
-      </aside>
-      <section>
-        <ITCConvertTool/>
-      </section>
-      <aside class="sidebar-advertisement sidebar-advertisement--right">
-        <ITCAdvertisement direction="vertical"/>
-      </aside>
+      <div class="page__wrapper">
+        <aside class="sidebar-advertisement sidebar-advertisement--left">
+          <ITCAdvertisement direction="vertical"/>
+        </aside>
+        <section>
+          <ITCConvertTool/>
+        </section>
+        <aside class="sidebar-advertisement sidebar-advertisement--right">
+          <ITCAdvertisement direction="vertical"/>
+        </aside>
+      </div>
     </div>
   </main>
 </template>
 
 <style lang="scss" scoped>
+.page {
+  &__wrapper {
+    position: relative;
+  }
+}
 .sidebar-advertisement {
+  position: sticky;
+  top: 50%;
 
+  &--left {
+    transform: translate(calc(-100% + 10px), -50%);
+  }
+
+  &--right {
+    transform: translate(calc(100% + 10px), -50%);
+  }
 }
 </style> 
