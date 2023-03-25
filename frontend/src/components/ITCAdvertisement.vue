4ccbe16296c8240a8fbfe8eacddd86751c8e32a5
Hyungmo Gu <guhyungm7@gmail.com>
2023-03-24 21:27:02 -0600

    feat: Creating frontend #147

--- frontend/src/components/ITCAdvertisement.vue
+++ frontend/src/components/ITCAdvertisement.vue
@@ -1,39 +1,10 @@
 
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
-</script>
-
 <template>
-  <div class="advertisement">
+  <div class="itc-advertisement">
     This is advertisement
   </div>
 </template>
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
-}
+<style lang="scss" scoped>
 
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
 </style>
--- frontend/src/components/ITCConvertTool.vue
+++ frontend/src/components/ITCConvertTool.vue
@@ -1,77 +1,26 @@
 <template>
-  <div class="item">
-    
+  <div class="itc-convert-tool">
+    <form class="itc-convert-tool__form form">
+      <fieldset>
+        <legend>Select format to convert to:</legend>
+        <div class="form__group form__group--radio">
+          <input type="radio" name="convert_to" value="JPG"/>
+          <label>JPG</label>
+        </div>
+        <div class="form__group form__group--radio">
+          <input type="radio" name="convert_to" value="PNG"/>
+          <label>PNG</label>
+        </div>
+        <div class="form__group form__group--radio">
+          <input type="radio" name="convert_to" value="WEBP"/>
+          <label>WEBP</label>
+        </div>
+      </fieldset>
+      <button class="form__button">Convert</button>
+    </form>
   </div>
 </template>
 
-<style scoped>
-.item {
-  margin-top: 2rem;
-  display: flex;
-}
+<style lang="scss" scoped>
 
-.details {
-  flex: 1;
-  margin-left: 1rem;
-}
-
-i {
-  display: flex;
-  place-items: center;
-  place-content: center;
-  width: 32px;
-  height: 32px;
-  color: var(--color-text);
-}
-
-h3 {
-  font-size: 1.2rem;
-  font-weight: 500;
-  margin-bottom: 0.4rem;
-  color: var(--color-heading);
-}
-
-@media (min-width: 1024px) {
-  .item {
-    margin-top: 0;
-    padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
-  }
-
-  i {
-    top: calc(50% - 25px);
-    left: -26px;
-    position: absolute;
-    border: 1px solid var(--color-border);
-    background: var(--color-background);
-    border-radius: 8px;
-    width: 50px;
-    height: 50px;
-  }
-
-  .item:before {
-    content: ' ';
-    border-left: 1px solid var(--color-border);
-    position: absolute;
-    left: 0;
-    bottom: calc(50% + 25px);
-    height: calc(50% - 25px);
-  }
-
-  .item:after {
-    content: ' ';
-    border-left: 1px solid var(--color-border);
-    position: absolute;
-    left: 0;
-    top: calc(50% + 25px);
-    height: calc(50% - 25px);
-  }
-
-  .item:first-of-type:before {
-    display: none;
-  }
-
-  .item:last-of-type:after {
-    display: none;
-  }
-}
 </style>
