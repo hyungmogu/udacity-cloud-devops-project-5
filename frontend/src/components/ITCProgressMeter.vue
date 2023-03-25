4c589f28e3a0798e88760eeaffcef8cd8e8fc10c
Hyungmo Gu <guhyungm7@gmail.com>
2023-03-25 10:36:32 -0600

    feat: Creating frontend #450

--- frontend/src/components/ITCProgressMeter.vue
+++ frontend/src/components/ITCProgressMeter.vue
@@ -1,3 +1,38 @@
 <template>
-    <div>This is progress meter</div>
+    <div class="itc-progress-meter">
+        <span class="itc-progress-meter__bar"></span>
+    </div>
 </template>
+
+<style lang="scss" scoped>
+@keyframes move {
+  0% {
+    background-position: 0 0;
+  }
+  100% {
+    background-position: 50px 50px;
+  }
+}
+.itc-progress-meter {
+    height: 20px;
+    background-color: var(--gray-2);
+    border-radius: 25px;
+    padding: 5px;
+    width: 200px;
+    box-shadow: inset 0 -1px 1px rgba(255, 255, 255, 0.3);
+
+    &__bar {
+        display: block;
+        height: 100%;
+        max-width: 20%;
+        border-top-right-radius: 8px;
+        border-bottom-right-radius: 8px;
+        border-top-left-radius: 20px;
+        border-bottom-left-radius: 20px;
+        background-color: var(--secondary);
+        position: relative;
+        box-shadow: inset 0 2px 9px rgba(255, 255, 255, 0.3), inset 0 -2px 6px rgba(0, 0, 0, 0.4);        overflow: hidden;
+    }
+}
+</style>
+
