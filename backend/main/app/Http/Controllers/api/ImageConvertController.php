1f3eaa0f52c028e74cca381a22eadbd51574f968
Hyungmo Gu <guhyungm7@gmail.com>
2023-03-28 07:52:33 -0600

    feat: Creating backend #1045

--- backend/main/app/Http/Controllers/api/ImageConvertController.php
+++ backend/main/app/Http/Controllers/api/ImageConvertController.php
@@ -21,11 +21,10 @@ class ImageConvertController extends Controller
     public function store(Request $request)
     {
         // convert image to target format
-
         // upload image to amazon s3
         // set time for it to be deleted after 15 minutes or 1 day (if this is only thing allowed)
-
         // return uploaded amazon s3 url back to user
+        
         $post = $request->all();
         $file = @$post['file'];
         $code = 200;
