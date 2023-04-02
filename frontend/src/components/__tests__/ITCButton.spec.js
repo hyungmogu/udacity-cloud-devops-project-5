b33b0bc0410176a4fb3b3534ff2358ece585918d
Hyungmo Gu <guhyungm7@gmail.com>
2023-04-02 10:36:37 -0600

    feat: Add dockerfile boilerplate #2

--- frontend/src/components/__tests__/ITCButton.spec.js
+++ frontend/src/components/__tests__/ITCButton.spec.js
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import ITCButton from '../ITCButton.vue'
 
-describe('HelloWorld', () => {
+describe('ITCButton', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
+    const wrapper = mount(ITCButton, { props: { msg: 'Hello Vitest' } })
     expect(wrapper.text()).toContain('Hello Vitest')
   })
 })
