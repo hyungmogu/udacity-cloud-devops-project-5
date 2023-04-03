083ac1c213e324d707e861d5ed06b5f978f46667
Hyungmo Gu <guhyungm7@gmail.com>
2023-04-03 07:36:35 -0600

    fix: Fix url not found error

--- backend/app/main/routes/api.py
+++ backend/app/main/routes/api.py
@@ -3,7 +3,7 @@ import main.controller.imgToJPGController as jpg
 import main.controller.imgToPNGController as png
 import main.controller.imgToWEBPController as webp
 
-api = Blueprint('api', __name__)
+api = Blueprint('api', __name__, url_prefix='/api/')
 
 api.route('/convert-to-jpg', methods=['POST'])(jpg.create_jpg)
 api.route('/convert-to-png', methods=['POST'])(png.create_png)
