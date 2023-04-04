bdf3c72804b1d50671336d637f717b427808fdc8
Hyungmo Gu <guhyungm7@gmail.com>
2023-04-03 19:35:53 -0600

    feat: Allow .env file to be imported from project root folder

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,12 @@
+import os
 from flask import Flask
 from dotenv import load_dotenv
 from main.routes.api import api
 
-load_dotenv()
+if os.environ.get('DOCKER_RUNNING', 'false') != 'true':
+    path = os.getcwd()
+    dotenv_path = os.path.dirname(path)
+    load_dotenv("{}/.env".format(dotenv_path))
 
 app = Flask(__name__)
 app.register_blueprint(api)
