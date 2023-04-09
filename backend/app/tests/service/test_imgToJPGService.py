05a818e1e325c1bcb06b17aa65617296b1fa6331
Hyungmo Gu <guhyungm7@gmail.com>
2023-04-09 00:08:23 -0600

    feat: Add simple positive tests for 'ImgToJPGServiceTest' for backend

---
+++ backend/app/tests/service/test_imgToJPGService.py
@@ -0,0 +1,12 @@
+import unittest
+from main.service.imgToJPGService import ImgToJPGService
+
+class ImgToJPGServiceTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_convert_method_successfully_converts_a_valid_image_to_jpg(self):
+        pass
+
+    def test_upload_method_uploads_converted_image_to_s3_bucket_and_returns_url(self):
+        pass 
