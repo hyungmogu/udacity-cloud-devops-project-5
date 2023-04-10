dd236d43beee6883740a19932b5c19c94c9f6d72
Hyungmo Gu <guhyungm7@gmail.com>
2023-04-09 22:59:44 -0600

    fix: Fix tests for 'ITCProgressMeter'
    Removed test cases that checks for background color. Vue test renders JSDOM, and doesn't render CSS.

--- frontend/src/components/__tests__/ITCProgressMeter.spec.js
+++ frontend/src/components/__tests__/ITCProgressMeter.spec.js
@@ -6,18 +6,6 @@ import ITCProgressMeter from '../ITCProgressMeter.vue'
 describe('ITCProgressMeter', () => {
   it('render properly with class props', () => {
     const wrapper = mount(ITCProgressMeter, { props: { class: 'complete' } });
-    expect(wrapper.text()).toContain('complete');
-  });
-
-  it('render properly with correct color given class complete', () => {
-    // checks if "background-color: var(--third)" is appied to the element ".itc-progress-meter__bar" 
-    const wrapper = mount(ITCProgressMeter, { props: { class: 'complete' } });
-    expect(wrapper.find('.itc-progress-meter__bar').attributes('style')).toContain('background-color: var(--third)');
-  });
-
-  it('render properly with correct color given class error', () => {
-    // checks if "background-color: var(--fourth)" is appied to the element ".itc-progress-meter__bar" 
-    const wrapper = mount(ITCProgressMeter, { props: { class: 'error' } });
-    expect(wrapper.find('.itc-progress-meter__bar').attributes('style')).toContain('background-color: var(--fourth)');
+    expect(wrapper.find(".itc-progress-meter").attributes('class')).toContain('complete');
   });
 })
