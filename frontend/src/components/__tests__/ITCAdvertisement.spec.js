a06b9a4a4c535b6b8d30e8e2e02adccf60ceb430
Hyungmo Gu <guhyungm7@gmail.com>
2023-04-10 20:59:02 -0600

    feat: Add test case for 'ITCAdvertisement'
    Added for 'google adsense props should be rendered properly'

--- frontend/src/components/__tests__/ITCAdvertisement.spec.js
+++ frontend/src/components/__tests__/ITCAdvertisement.spec.js
@@ -1,24 +1,42 @@
-
 // it should test the following:
 // 1. on load, it should add google adsense script to <head>
 // 2. google adsense script in <head> should be loaded only once
 // 3. google adsense should be rendered properly
 
-import { describe, it, expect } from 'vitest'
+import {
+  describe,
+  it,
+  expect
+} from 'vitest'
 
-import { mount } from '@vue/test-utils'
+import {
+  mount
+} from '@vue/test-utils'
 import ITCAdvertisement from '../ITCAdvertisement.vue'
 
 describe('ITCAdvertisement', () => {
-  it('should add google adsense script to <head>', () => {
-
-  });
+  it('should add google adsense script to <head>', () => {});
 
   it('google adsense script in <head> should be loaded only once', () => {
 
   });
 
-  it('google adsense should be rendered properly', () => {
-   
+  it('google adsense props should be rendered properly', () => {
+    const wrapper = mount(ITCAdvertisement, {
+      props: {
+        "data-ad-client": 'test1',
+        'data-ad-slot': '12345',
+        'data-ad-format': 'auto',
+        'data-full-width-response': 'false'
+      }
+    });
+
+    expect(wrapper.find('script').attributes('src')).toContain('pagead2.googlesyndication.com/pagead/js/adsbygoogle.js');
+    expect(wrapper.find('ins').attributes('class')).toContain('adsbygoogle');
+    expect(wrapper.find('script').attributes('src')).toContain('client=test1');
+    expect(wrapper.find('ins').attributes('data-ad-client')).toContain('test1');
+    expect(wrapper.find('ins').attributes('data-ad-slot')).toContain('12345');
+    expect(wrapper.find('ins').attributes('data-ad-format')).toContain('auto');
+    expect(wrapper.find('ins').attributes('data-full-width-response')).toContain('false');
   });
 });
