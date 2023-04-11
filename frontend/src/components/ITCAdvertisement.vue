2ed719192793aaa00c0e8893335b8eda9f8ace52
Hyungmo Gu <guhyungm7@gmail.com>
2023-04-10 21:57:55 -0600

    feat: Define core code
    Added core code for ITCAdvertisement.

--- frontend/src/components/ITCAdvertisement.vue
+++ frontend/src/components/ITCAdvertisement.vue
@@ -3,11 +3,56 @@
   <div class="itc-advertisement">
     <small class="itc-advertisement__title">This is advertisement</small>
     <div class="itc-advertisement__banner">
-
+      <!-- Homepage Leaderboard -->
+      <ins class="adsbygoogle" v-bind:class="className"
+      v-bind:data-ad-client="dataAdClient"
+      v-bind:data-ad-slot="dataAdSlot"></ins>
     </div>
   </div>
 </template>
-
+<script>
+export default {
+  props:{
+    className:{
+      type:String,
+      default:'',
+    },
+    dataAdClient:{
+      type:String,
+      default:'',
+    },
+    dataAdSlot: {
+     type: String,
+     default: ''
+    },
+    dataAdFormat: {
+     type: String,
+     default: 'auto'
+    },
+    dataFullWidthResponse: {
+      type: String,
+      default: 'false'
+    }
+  },
+  mounted() {
+    this.handleLoadScriptTag();
+    this.handleUpdateAdsbygoogle();
+  },
+  methods: {
+    handleLoadScriptTag() {
+      const script = document.createElement('script');
+      script.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${this.dataAdClient}`;
+      script.async = true;
+      script.crossOrigin = 'anonymous';
+      document.head.appendChild(script);
+    },
+    handleUpdateAdsbygoogle() {
+      window.adsbygoogle = window.adsbygoogle || [];
+      window.adsbygoogle.push({});
+    }
+  }
+};
+</script>
 <style lang="scss" scoped>
 .itc-advertisement {
   &__title {
