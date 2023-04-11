4db2477249df1dbba64988ee820a169b11e6a872
Hyungmo Gu <guhyungm7@gmail.com>
2023-04-10 19:37:09 -0600

    feat: Add tests templates for 'ITCAdvertisement.vue'

---
+++ frontend/src/components/__tests__/ITCAdvertisement.spec.js
@@ -0,0 +1,24 @@
+
+// it should test the following:
+// 1. on load, it should add google adsense script to <head>
+// 2. google adsense script in <head> should be loaded only once
+// 3. google adsense should be rendered properly
+
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import ITCAdvertisement from '../ITCAdvertisement.vue'
+
+describe('ITCAdvertisement', () => {
+  it('should add google adsense script to <head>', () => {
+
+  });
+
+  it('google adsense script in <head> should be loaded only once', () => {
+
+  });
+
+  it('google adsense should be rendered properly', () => {
+   
+  });
+});
