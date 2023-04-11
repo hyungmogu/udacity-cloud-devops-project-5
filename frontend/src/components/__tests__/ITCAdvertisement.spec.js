d074e6f774631c773dc3139c1035251f99ac565d
Hyungmo Gu <guhyungm7@gmail.com>
2023-04-10 21:28:50 -0600

    refactor: remove extraneous tests.
    comparing the two instructions (first: https://support.google.com/adsense/answer/9274516?hl=en) (second: https://support.google.com/adsense/answer/9190028?hl=en), there seems no need to add <script> tag to <head>

--- frontend/src/components/__tests__/ITCAdvertisement.spec.js
+++ frontend/src/components/__tests__/ITCAdvertisement.spec.js
@@ -15,12 +15,6 @@ import {
 import ITCAdvertisement from '../ITCAdvertisement.vue'
 
 describe('ITCAdvertisement', () => {
-  it('should add google adsense script to <head>', () => {});
-
-  it('google adsense script in <head> should be loaded only once', () => {
-
-  });
-
   it('google adsense props should be rendered properly', () => {
     const wrapper = mount(ITCAdvertisement, {
       props: {
