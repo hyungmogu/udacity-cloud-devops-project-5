9511e19a1049ab52315d34aeb26a71aacb1f27f8
Hyungmo Gu <guhyungm7@gmail.com>
2023-04-10 23:03:49 -0600

    refactor: Remove commented codes

--- frontend/src/components/__tests__/ITCAdvertisement.spec.js
+++ frontend/src/components/__tests__/ITCAdvertisement.spec.js
@@ -1,8 +1,3 @@
-// it should test the following:
-// 1. on load, it should add google adsense script to <head>
-// 2. google adsense script in <head> should be loaded only once
-// 3. google adsense should be rendered properly
-
 import {
   describe,
   it,
