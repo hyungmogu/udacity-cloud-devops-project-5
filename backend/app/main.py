fa0112ab8cad0df4dcfcc4cbdd5d17424dfc72d0
Hyungmo Gu <guhyungm7@gmail.com>
2023-06-07 20:14:29 -0600

    feat: update file
    Replaced 'main.py' in backend. Replacing flask with fastapi. flask is realllllly hard to make it work in production.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,19 +1,16 @@
-import os
-from flask import Flask
-from dotenv import load_dotenv
-from src.routes.api import api
+import uvicorn
+from fastapi import FastAPI
 
-if os.environ.get('DOCKER_RUNNING', 'false') != 'true':
-    path = os.getcwd()
-    dotenv_path = os.path.dirname(path)
-    load_dotenv("{}/.env".format(dotenv_path))
+from src.database.connections import Database
+from src.routers.users import  convert_router
 
-app = Flask(__name__)
-app.register_blueprint(api)
+app = FastAPI()
 
-@app.route('/')
-def index():
-    return 'This app is working'
+app.include_router(convert_router, prefix="/convert", tags=["Convert"])
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
+@app.on_event("startup")
+async def on_startup():
+    Database()
+
+if __name__ == '__main__':
+    uvicorn.run("app:app", host="0.0.0.0", port=8004, reload=True)
