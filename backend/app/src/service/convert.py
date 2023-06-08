8be1c3b3b60583616e367950e26d32be3bfa8fcd
Hyungmo Gu <guhyungm7@gmail.com>
2023-06-08 08:10:24 -0600

    feat: update file
    Added base code for 'convert.py' in 'service' for 'backend'.

---
+++ backend/app/src/service/convert.py
@@ -0,0 +1,33 @@
+from PIL import Image
+from io import BytesIO
+import os
+import logging
+import boto3
+from abc import ABC, abstractmethod
+from uuid import uuid4
+from time import time
+from botocore.exceptions import ClientError
+
+
+AWS_S3_BUCKET = os.environ.get("AWS_S3_BUCKET", "")
+AWS_OBJECT_EXPIRES_IN = int(os.environ.get("AWS_OBJECT_EXPIRES_IN", "0"))
+
+class Convert(ABC):
+  def upload(self, new_img) -> dict:
+    file_name = "{}-{}.jpeg".format(time(), uuid4().hex)
+    response = None
+
+    try:
+      s3_client = boto3.client("s3")
+      s3_client.upload_fileobj(new_img, AWS_S3_BUCKET, file_name)
+
+      response = s3_client.generate_presigned_url(
+        "get_object",
+        Params={"Bucket": AWS_S3_BUCKET, "Key": file_name},
+        ExpiresIn=AWS_OBJECT_EXPIRES_IN,
+      )
+
+    except ClientError as e:
+      logging.error(e)
+
+    return response
