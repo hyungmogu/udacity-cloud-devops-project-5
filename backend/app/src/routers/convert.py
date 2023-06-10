ac4f4717dd0866eaa964f05d0a4c49a76e78a689
Hyungmo Gu <guhyungm7@gmail.com>
2023-06-09 23:13:49 -0600

    feat: updated file
    refined function 'convert_to_jpg' under 'routers'.

--- backend/app/src/routers/convert.py
+++ backend/app/src/routers/convert.py
@@ -2,7 +2,7 @@ from typing import List, Optional
 from fastapi import APIRouter, Depends, HTTPException, status
 
 from src.database.connections import get_session
-from src.services.convert import ConvertServices
+from src.services.convert import ImgToJPGService
 
 
 convert_router = APIRouter(
@@ -11,9 +11,10 @@ convert_router = APIRouter(
 
 
 @convert_router.post('/jpg', response_model=ConvertedImage, status_code=201)
-async def convert_to_jpg(file: UploadFile) -> ConvertedImage:
-    service:ConvertServices = ConvertServices()
-    result:ConvertedImage = service.convert_to_jpg(file)
+async def convert_to_jpg(file: UploadFile):
+    service = ImgToJPGService()
+    in_mem_file = service.convert_to_jpg(file)
+    result = service.upload_file(in_mem_file, 'jpg')
     return result
 
 @convert_router.post('/png', response_model=ConvertedImage, status_code=201)
