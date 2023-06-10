dde0f038b7b273fc17657d7d16134d87e3d81c34
Hyungmo Gu <guhyungm7@gmail.com>
2023-06-10 10:58:32 -0600

    fix: fix backend
    fixed issue "sqlalchemy.exc.ArgumentError: Mapper mapped class ConvertedImage->convertedimage could not assemble any primary key columns for mapped table 'convertedimage'".

--- backend/app/src/models/convert.py
+++ backend/app/src/models/convert.py
@@ -2,12 +2,14 @@ from datetime import datetime
 from sqlmodel import SQLModel, Field
 
 class ConvertedImage(SQLModel, table=True):
+    id:int = Field(default=None, primary_key=True)
     url:str
     converted_format:str
 
     class Config:
         schema_extra = {
             "example": {
+                "id": 1,
                 "url": "AWS_S3_URL_GOES_HERE",
                 "converted_format": "jpg"
             }
