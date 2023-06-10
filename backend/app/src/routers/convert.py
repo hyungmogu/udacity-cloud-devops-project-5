f6234e8a9f3748d8cd4f64b969d5107baf6d3fec
Hyungmo Gu <guhyungm7@gmail.com>
2023-06-10 12:13:24 -0600

    feat: update backend
    updated backend path. Done to make it compatible to fastapi.

--- backend/app/src/routers/convert.py
+++ backend/app/src/routers/convert.py
@@ -8,21 +8,21 @@ convert_router = APIRouter(
     tags=["Convert"]
 )
 
-@convert_router.post('/jpg', response_model=ConvertedImage, status_code=201)
+@convert_router.post('/to-jpg', response_model=ConvertedImage, status_code=201)
 async def convert_to_jpg(file: UploadFile):
     service = ImgToJPGService()
     in_mem_file = service.convert_to_jpg(file)
     result = service.upload_file(in_mem_file, 'jpg')
     return result
 
-@convert_router.post('/png', response_model=ConvertedImage, status_code=201)
+@convert_router.post('/to-png', response_model=ConvertedImage, status_code=201)
 async def convert_to_png(file: UploadFile):
     service = ImgToPNGService()
     in_mem_file = service.convert_to_png(file)
     result = service.upload_file(in_mem_file, 'png')
     return result
 
-@convert_router.post('/webp', response_model=ConvertedImage, status_code=201)
+@convert_router.post('/to-webp', response_model=ConvertedImage, status_code=201)
 async def convert_to_webp(file: UploadFile):
     service = ImgToWEBPService()
     in_mem_file = service.convert_to_webp(file)
