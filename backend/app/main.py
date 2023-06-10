4558c026a499ebf1a33e2afe7616479417e828da
Hyungmo Gu <guhyungm7@gmail.com>
2023-06-10 10:33:45 -0600

    fix: fix backend
    fixed "ImportError: cannot import name 'Database' from 'src.database.connections'".

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 
-from src.database.connections import Database
+from src.database.connections import conn
 from src.routers.convert import convert_router
 from src.routers.health import health_router
 
@@ -12,7 +12,7 @@ app.include_router(health_router, prefix="/health", tags=["Health"])
 
 @app.on_event("startup")
 async def on_startup():
-    Database()
+    conn()
 
 if __name__ == '__main__':
     uvicorn.run("app:app", host="0.0.0.0", port=8004, reload=True)
