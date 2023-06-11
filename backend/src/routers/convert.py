164f46fe62034e6acd2c217722a8aab6f2daa4c3
Hyungmo Gu <guhyungm7@gmail.com>
2023-06-11 12:35:21 -0600

    feat: update backend
    Added exceptions to 'routes/convert.py'.

--- backend/src/routers/convert.py
+++ backend/src/routers/convert.py
@@ -10,6 +10,13 @@ convert_router = APIRouter(
 
 @convert_router.post('/to-jpg', response_model=str, status_code=201)
 async def convert_to_jpg(image: UploadFile):
+
+    if image is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="No image provided.")
+
+    if image.content_type != 'image/jpeg':
+        raise HTTPException(status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE, detail="Only JPEG/JPG images are supported.")
+
     try:
         image_binary = await image.read()
         buffer = BytesIO(image_binary)
@@ -20,6 +27,10 @@ async def convert_to_jpg(image: UploadFile):
     service = ImgToJPGService()
     in_mem_file = service.convert(buffer)
     result = service.upload(in_mem_file, 'jpg')
+
+    if result is None:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Error uploading image.")
+
     return result
 
 @convert_router.post('/to-png', response_model=str, status_code=201)
