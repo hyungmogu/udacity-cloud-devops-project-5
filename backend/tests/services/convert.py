8121b35eb721d628ef942376b087f16fda63e5ca
Hyungmo Gu <guhyungm7@gmail.com>
2023-06-12 07:15:29 -0600

    feat: update backend
    Added test for health check. The purpose of this test is to make sure testing server is running.

--- backend/tests/services/convert.py
+++ backend/tests/services/convert.py
@@ -16,6 +16,16 @@ os.environ["AWS_SECRET_ACCESS_KEY"] = "test"
 from app import app
 from src.services.convert import ImgToJPGService, ImgToPNGService, ImgToWEBPService
 
+# HEALTH CHECK
+
+class TestHealthCheck(unittest.TestCase):
+    def setUp(self):
+        self.app = TestClient(app)
+
+    def test_test_client_server_is_running(self):
+        response = self.app.get("/health")
+        self.assertEqual(response.status_code, 200)
+
 # TESTING IMG TO JPG SERVICE
 
 @mock_s3
