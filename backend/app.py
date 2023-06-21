d7ebe8f5223d761f0441ef03a1c760c019ee8680
Hyungmo Gu <guhyungm7@gmail.com>
2023-06-21 07:37:28 -0600

    feat: update backend
    disable the initial setup of database. Database is not used on this app.

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,6 @@ import uvicorn
 import logging
 from fastapi import FastAPI
 
-from src.database.connections import conn
 from src.routers.convert import convert_router
 from src.routers.health import health_router
 
@@ -13,9 +12,5 @@ app = FastAPI()
 app.include_router(convert_router, prefix="/convert", tags=["Convert"])
 app.include_router(health_router, prefix="/health", tags=["Health"])
 
-@app.on_event("startup")
-async def on_startup():
-    conn()
-
 if __name__ == '__main__':
-    uvicorn.run("app:app", host="0.0.0.0", port=8004, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8004, reload=True) 
