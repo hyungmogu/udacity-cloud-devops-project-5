1850e99945b14fc85f9153646152217431df8d33
Hyungmo Gu <guhyungm7@gmail.com>
2023-06-27 07:37:25 -0600

    feat: update backend
    Move codes about starting virtual environment from `Makefile` to `minikube.sh`.

--- backend/minikube.sh
+++ backend/minikube.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+python3 -m venv ./venv &&\
+./venv/bin/pip install python-dotenv==1.0.0
 ./venv/bin/python3 ./prepare_kubernetes.py && \
 minikube start && \
 kubectl apply -f ../.circleci/kubernetes/base_src/
