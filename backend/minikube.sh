f3d899489bdfa7c60ddff2bf3d14cb54d6119cc0
Hyungmo Gu <guhyungm7@gmail.com>
2023-06-28 00:42:50 -0600

    feat: update kubernetes
    removed memory from `deployment.yaml.example` and `hpa.yaml.example`. Done to simplify troubleshooting. The author is trying to make current resource metrics to not appear as `<unkown>`.

--- backend/minikube.sh
+++ backend/minikube.sh
@@ -4,3 +4,4 @@ python3 -m venv ./venv &&\
 ./venv/bin/python3 ./prepare_kubernetes.py && \
 minikube start && \
 kubectl apply -f ../.circleci/kubernetes/base_src/
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
