10e9179350334c61c79243d4f5b511eb1b0ef5a5
Hyungmo Gu <guhyungm7@gmail.com>
2023-07-02 21:41:16 -0600

    feat: update microservices
    Added httpx setup code to `gateway` microservice component.

--- microservices/gateway/app.py
+++ microservices/gateway/app.py
@@ -12,5 +12,15 @@ app = FastAPI()
 app.include_router(convert_router, prefix="/convert", tags=["Convert"])
 app.include_router(health_router, prefix="/health", tags=["Health"])
 
+@app.on_event("startup")
+async def startup_event():
+    httpx_client_wrapper.start()
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    await httpx_client_wrapper.stop()
+
+
 if __name__ == '__main__':
     uvicorn.run("app:app", host="0.0.0.0", port=8010, reload=True) 
