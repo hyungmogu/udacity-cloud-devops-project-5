b237669910e02ce98a3dcdf7ac62fdfd4368b73f
Hyungmo Gu <guhyungm7@gmail.com>
2023-07-03 07:50:19 -0600

    feat: update microservices
    added code block for `convert_to_jpg` function in `gateway` microservices component.

--- microservices/gateway/src/routers/convert.py
+++ microservices/gateway/src/routers/convert.py
@@ -1,7 +1,7 @@
 from io import BytesIO
-import httpx
 import logging
 from fastapi import APIRouter, HTTPException, status, UploadFile
+from src.utils.httpx import httpx_client_wrapper
 
 from src.services.convert import ImgToJPGService, ImgToPNGService, ImgToWEBPService
 
@@ -10,8 +10,16 @@ convert_router = APIRouter(
 )
 
 @convert_router.post('/to-jpg', response_model=str, status_code=201)
-async def convert_to_jpg(image: UploadFile):
-    pass
+async def convert_to_jpg(image: UploadFile, url: str = None):
+    # @TODO: Add code that checks for rate limit
+
+    async_client = httpx_client_wrapper()
+    res = await async_client.post(url, files={'file': image.file})
+    result = res.text
+    return {
+        'result': result,
+        'status': res.status_code
+    }
 
 @convert_router.post('/to-png', response_model=str, status_code=201)
 async def convert_to_png(image: UploadFile):
