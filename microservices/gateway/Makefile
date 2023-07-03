cd9429adc9e4404a051d1339411c05fc63ec0d3d
Hyungmo Gu <guhyungm7@gmail.com>
2023-07-02 19:09:54 -0600

    feat: Add files grateway
    Added basic files for gateway. Gateway is the entrypoint to the application. It serves the front end and provides a REST API.

---
+++ microservices/gateway/Makefile
@@ -0,0 +1,3 @@
+start:
+	cd main &&\
+	python main.py
---
+++ microservices/gateway/app.py
@@ -0,0 +1,16 @@
+import uvicorn
+import logging
+from fastapi import FastAPI
+
+from src.routers.convert import convert_router
+from src.routers.health import health_router
+
+logging.basicConfig(level=logging.DEBUG)
+
+app = FastAPI()
+
+app.include_router(convert_router, prefix="/convert", tags=["Convert"])
+app.include_router(health_router, prefix="/health", tags=["Health"])
+
+if __name__ == '__main__':
+    uvicorn.run("app:app", host="0.0.0.0", port=8010, reload=True) 
