1bc2070b6f032014515a7ff45ce986187589bf37
Hyungmo Gu <guhyungm7@gmail.com>
2023-07-07 21:29:56 -0600

    feat: update progress
    - Resolved error `AttributeError: 'URL' object has no attribute 'decode'` for `gateway` component. This is caused by forgetting to convert to string version of request.url. request.url is an URL object.
    - Resolved error `httpx.UnsupportedProtocol: Request URL is missing an 'http://' or 'https://' protocol.` for `gateway` component. Added `SERVER_PROTOCOL` to .env.example file for this reason.

--- microservices/gateway/src/routers/convert.py
+++ microservices/gateway/src/routers/convert.py
@@ -1,7 +1,7 @@
 from urllib.parse import urlparse
 from fastapi import Depends, APIRouter, UploadFile, Request
 from src.utils.httpx import httpx_client_wrapper
-from config import API_MAX_REQUESTS_PER_DAY, API_SECONDS_IN_DAY, SERVER_JPG_HOST, SERVER_PNG_HOST, SERVER_WEBP_HOST
+from config import API_MAX_REQUESTS_PER_DAY, API_SECONDS_IN_DAY, SERVER_JPG_HOST, SERVER_PNG_HOST, SERVER_WEBP_HOST, SERVER_PROTOCOL
 from fastapi_limiter.depends import RateLimiter
 
 convert_router = APIRouter(
@@ -10,8 +10,7 @@ convert_router = APIRouter(
 
 @convert_router.post('/to-jpg', response_model=str, dependencies=[Depends(RateLimiter(times=API_MAX_REQUESTS_PER_DAY, seconds=int(API_SECONDS_IN_DAY)))], status_code=201)
 async def convert_to_jpg(image: UploadFile, request: Request):
-    print(urlparse(request.url).path)
-    outbound_url = "{}{}".format(SERVER_JPG_HOST, urlparse(request.url).path)
+    outbound_url = "{}://{}{}".format(SERVER_PROTOCOL, SERVER_JPG_HOST, urlparse(str(request.url)).path)
     async_client = httpx_client_wrapper()
     res = await async_client.post(outbound_url, files={'file': image.file})
     result = res.text
@@ -19,7 +18,7 @@ async def convert_to_jpg(image: UploadFile, request: Request):
 
 @convert_router.post('/to-png', response_model=str, dependencies=[Depends(RateLimiter(times=API_MAX_REQUESTS_PER_DAY, seconds=int(API_SECONDS_IN_DAY)))], status_code=201)
 async def convert_to_png(image: UploadFile, request: Request):
-    outbound_url = "{}{}".format(SERVER_PNG_HOST, urlparse(request.url).path)
+    outbound_url = "{}://{}{}".format(SERVER_PROTOCOL, SERVER_JPG_HOST, urlparse(str(request.url)).path)
     async_client = httpx_client_wrapper()
     res = await async_client.post(outbound_url, files={'file': image.file})
     result = res.text
@@ -27,7 +26,7 @@ async def convert_to_png(image: UploadFile, request: Request):
 
 @convert_router.post('/to-webp', response_model=str, dependencies=[Depends(RateLimiter(times=API_MAX_REQUESTS_PER_DAY, seconds=int(API_SECONDS_IN_DAY)))], status_code=201)
 async def convert_to_webp(image: UploadFile, request: Request):
-    outbound_url = "{}{}".format(SERVER_WEBP_HOST, urlparse(request.url).path)
+    outbound_url = "{}://{}{}".format(SERVER_PROTOCOL, SERVER_JPG_HOST, urlparse(str(request.url)).path)
     async_client = httpx_client_wrapper()
     res = await async_client.post(outbound_url, files={'file': image.file})
     result = res.text
