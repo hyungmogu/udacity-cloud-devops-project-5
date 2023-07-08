1e288d6e14a09cc14f802f4f3134097861a68611
Hyungmo Gu <guhyungm7@gmail.com>
2023-07-07 20:51:44 -0600

    feat: update progress
    - Updated `REDIS_HOST_LOCAL` to solve error `redis.exceptions.ConnectionError: Error connecting to localhost:6379. Multiple exceptions: [Errno 111] Connect call failed ('127.0.0.1', 6379), [Errno 99] Cannot assign requested address.`
    - Added outbound endpoints for `gateway` component. These endpoints will be used to communicate with `server-jpg`, `server-png` and `server-webp`. Its response will be forwarded back to user.

--- microservices/gateway/src/routers/convert.py
+++ microservices/gateway/src/routers/convert.py
@@ -1,6 +1,7 @@
-from fastapi import Depends, APIRouter, UploadFile
+from urllib.parse import urlparse
+from fastapi import Depends, APIRouter, UploadFile, Request
 from src.utils.httpx import httpx_client_wrapper
-from config import API_MAX_REQUESTS_PER_DAY, API_SECONDS_IN_DAY
+from config import API_MAX_REQUESTS_PER_DAY, API_SECONDS_IN_DAY, SERVER_JPG_HOST, SERVER_PNG_HOST, SERVER_WEBP_HOST
 from fastapi_limiter.depends import RateLimiter
 
 convert_router = APIRouter(
@@ -8,22 +9,26 @@ convert_router = APIRouter(
 )
 
 @convert_router.post('/to-jpg', response_model=str, dependencies=[Depends(RateLimiter(times=API_MAX_REQUESTS_PER_DAY, seconds=int(API_SECONDS_IN_DAY)))], status_code=201)
-async def convert_to_jpg(image: UploadFile, url: str = None):
+async def convert_to_jpg(image: UploadFile, request: Request):
+    print(urlparse(request.url).path)
+    outbound_url = "{}{}".format(SERVER_JPG_HOST, urlparse(request.url).path)
     async_client = httpx_client_wrapper()
-    res = await async_client.post(url, files={'file': image.file})
+    res = await async_client.post(outbound_url, files={'file': image.file})
     result = res.text
     return result
 
 @convert_router.post('/to-png', response_model=str, dependencies=[Depends(RateLimiter(times=API_MAX_REQUESTS_PER_DAY, seconds=int(API_SECONDS_IN_DAY)))], status_code=201)
-async def convert_to_png(image: UploadFile, url: str = None):
+async def convert_to_png(image: UploadFile, request: Request):
+    outbound_url = "{}{}".format(SERVER_PNG_HOST, urlparse(request.url).path)
     async_client = httpx_client_wrapper()
-    res = await async_client.post(url, files={'file': image.file})
+    res = await async_client.post(outbound_url, files={'file': image.file})
     result = res.text
     return result
 
 @convert_router.post('/to-webp', response_model=str, dependencies=[Depends(RateLimiter(times=API_MAX_REQUESTS_PER_DAY, seconds=int(API_SECONDS_IN_DAY)))], status_code=201)
-async def convert_to_webp(image: UploadFile, url: str = None):
+async def convert_to_webp(image: UploadFile, request: Request):
+    outbound_url = "{}{}".format(SERVER_WEBP_HOST, urlparse(request.url).path)
     async_client = httpx_client_wrapper()
-    res = await async_client.post(url, files={'file': image.file})
+    res = await async_client.post(outbound_url, files={'file': image.file})
     result = res.text
     return result
