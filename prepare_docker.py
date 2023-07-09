8f9108c1485f736f46e0175ceb3d60c390264f1b
Hyungmo Gu <guhyungm7@gmail.com>
2023-07-08 22:01:43 -0600

    feat: update progress
    - Turned setup code in `prepare_docker.py` into an object

--- prepare_docker.py
+++ prepare_docker.py
@@ -3,77 +3,83 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-def run():
-    env_variable_groups = [
-    {   
-        "for": ["gateway"],
-        "values": {
-            "API_MAX_REQUESTS_PER_DAY": os.environ.get("API_MAX_REQUESTS_PER_DAY", "5"),
-            "API_SECONDS_IN_DAY": os.environ.get("API_SECONDS_IN_DAY", "86400"),
-            "REDIS_HOST": os.environ.get("REDIS_HOST", ""),
-            "GATEWAY_PORT": os.environ.get("GATEWAY_PORT", ""),
-            "SERVER_PROTOCOL": os.environ.get("SERVER_PROTOCOL", ""),
-            "SERVER_JPG_HOST": os.environ.get("SERVER_JPG_HOST", ""),
-            "SERVER_JPG_PORT": os.environ.get("SERVER_JPG_PORT", ""),
-            "SERVER_PNG_HOST": os.environ.get("SERVER_PNG_HOST", ""),
-            "SERVER_PNG_PORT": os.environ.get("SERVER_PNG_PORT", ""),
-            "SERVER_WEBP_HOST": os.environ.get("SERVER_WEBP_HOST", ""),
-            "SERVER_WEBP_PORT": os.environ.get("SERVER_WEBP_PORT", "")
-        }
-    },{
-        "for": ["server-jpg"],
-        "values": {
-            "SERVER_JPG_HOST": os.environ.get("SERVER_JPG_HOST", ""),
-            "SERVER_JPG_PORT": os.environ.get("SERVER_JPG_PORT", ""),
-            "AWS_S3_BUCKET": os.environ.get("AWS_S3_BUCKET", ""),
-            "AWS_OBJECT_EXPIRES_IN": int(os.environ.get("AWS_OBJECT_EXPIRES_IN", "0")),
-            "AWS_ACCESS_KEY_ID": os.environ.get("AWS_ACCESS_KEY_ID", ""),
-            "AWS_SECRET_ACCESS_KEY": os.environ.get("AWS_SECRET_ACCESS_KEY", ""),
-            "AWS_DEFAULT_REGION": os.environ.get("AWS_DEFAULT_REGION", "")
-        }
-    }, {
-        "for": [ "server-png"],
-        "values": {
-            "SERVER_PNG_HOST": os.environ.get("SERVER_PNG_HOST", ""),
-            "SERVER_PNG_PORT": os.environ.get("SERVER_PNG_PORT", ""),
-            "AWS_S3_BUCKET": os.environ.get("AWS_S3_BUCKET", ""),
-            "AWS_OBJECT_EXPIRES_IN": int(os.environ.get("AWS_OBJECT_EXPIRES_IN", "0")),
-            "AWS_ACCESS_KEY_ID": os.environ.get("AWS_ACCESS_KEY_ID", ""),
-            "AWS_SECRET_ACCESS_KEY": os.environ.get("AWS_SECRET_ACCESS_KEY", ""),
-            "AWS_DEFAULT_REGION": os.environ.get("AWS_DEFAULT_REGION", "")
-        }
-    }, {
-        "for": ["server-webp"],
-        "values": {
-            "SERVER_WEBP_HOST": os.environ.get("SERVER_WEBP_HOST", ""),
-            "SERVER_WEBP_PORT": os.environ.get("SERVER_WEBP_PORT", ""),
-            "AWS_S3_BUCKET": os.environ.get("AWS_S3_BUCKET", ""),
-            "AWS_OBJECT_EXPIRES_IN": int(os.environ.get("AWS_OBJECT_EXPIRES_IN", "0")),
-            "AWS_ACCESS_KEY_ID": os.environ.get("AWS_ACCESS_KEY_ID", ""),
-            "AWS_SECRET_ACCESS_KEY": os.environ.get("AWS_SECRET_ACCESS_KEY", ""),
-            "AWS_DEFAULT_REGION": os.environ.get("AWS_DEFAULT_REGION", "")
-        }
-    }]
+class DockerComposeSetupTool:
+    def prepare_docker_environment_variables(self):
+        env_variable_groups = [
+        {   
+            "for": ["gateway"],
+            "values": {
+                "API_MAX_REQUESTS_PER_DAY": os.environ.get("API_MAX_REQUESTS_PER_DAY", "5"),
+                "API_SECONDS_IN_DAY": os.environ.get("API_SECONDS_IN_DAY", "86400"),
+                "REDIS_HOST": os.environ.get("REDIS_HOST", ""),
+                "GATEWAY_PORT": os.environ.get("GATEWAY_PORT", ""),
+                "SERVER_PROTOCOL": os.environ.get("SERVER_PROTOCOL", ""),
+                "SERVER_JPG_HOST": os.environ.get("SERVER_JPG_HOST", ""),
+                "SERVER_JPG_PORT": os.environ.get("SERVER_JPG_PORT", ""),
+                "SERVER_PNG_HOST": os.environ.get("SERVER_PNG_HOST", ""),
+                "SERVER_PNG_PORT": os.environ.get("SERVER_PNG_PORT", ""),
+                "SERVER_WEBP_HOST": os.environ.get("SERVER_WEBP_HOST", ""),
+                "SERVER_WEBP_PORT": os.environ.get("SERVER_WEBP_PORT", "")
+            }
+        },{
+            "for": ["server-jpg"],
+            "values": {
+                "SERVER_JPG_HOST": os.environ.get("SERVER_JPG_HOST", ""),
+                "SERVER_JPG_PORT": os.environ.get("SERVER_JPG_PORT", ""),
+                "AWS_S3_BUCKET": os.environ.get("AWS_S3_BUCKET", ""),
+                "AWS_OBJECT_EXPIRES_IN": int(os.environ.get("AWS_OBJECT_EXPIRES_IN", "0")),
+                "AWS_ACCESS_KEY_ID": os.environ.get("AWS_ACCESS_KEY_ID", ""),
+                "AWS_SECRET_ACCESS_KEY": os.environ.get("AWS_SECRET_ACCESS_KEY", ""),
+                "AWS_DEFAULT_REGION": os.environ.get("AWS_DEFAULT_REGION", "")
+            }
+        }, {
+            "for": [ "server-png"],
+            "values": {
+                "SERVER_PNG_HOST": os.environ.get("SERVER_PNG_HOST", ""),
+                "SERVER_PNG_PORT": os.environ.get("SERVER_PNG_PORT", ""),
+                "AWS_S3_BUCKET": os.environ.get("AWS_S3_BUCKET", ""),
+                "AWS_OBJECT_EXPIRES_IN": int(os.environ.get("AWS_OBJECT_EXPIRES_IN", "0")),
+                "AWS_ACCESS_KEY_ID": os.environ.get("AWS_ACCESS_KEY_ID", ""),
+                "AWS_SECRET_ACCESS_KEY": os.environ.get("AWS_SECRET_ACCESS_KEY", ""),
+                "AWS_DEFAULT_REGION": os.environ.get("AWS_DEFAULT_REGION", "")
+            }
+        }, {
+            "for": ["server-webp"],
+            "values": {
+                "SERVER_WEBP_HOST": os.environ.get("SERVER_WEBP_HOST", ""),
+                "SERVER_WEBP_PORT": os.environ.get("SERVER_WEBP_PORT", ""),
+                "AWS_S3_BUCKET": os.environ.get("AWS_S3_BUCKET", ""),
+                "AWS_OBJECT_EXPIRES_IN": int(os.environ.get("AWS_OBJECT_EXPIRES_IN", "0")),
+                "AWS_ACCESS_KEY_ID": os.environ.get("AWS_ACCESS_KEY_ID", ""),
+                "AWS_SECRET_ACCESS_KEY": os.environ.get("AWS_SECRET_ACCESS_KEY", ""),
+                "AWS_DEFAULT_REGION": os.environ.get("AWS_DEFAULT_REGION", "")
+            }
+        }]
+        
+        # for each file in microservices folder (except 'server')
+        # delete traditional .env file and create a new one with the same name
+        for folder in os.listdir("microservices"):
+            if folder == "server":
+                continue
+            
+            if os.path.exists("microservices/{}/.env".format(folder)):
+                os.remove("microservices/{}/.env".format(folder))
+            
+            # open utf-8 encoding
+            with open("microservices/{}/.env".format(folder), "w+", encoding="utf-8") as f:
+                # for each env_variable_group
+                for env_variable_group in env_variable_groups:
+                    # if the folder is in the env_variable_group
+                    if folder in env_variable_group["for"]:
+                        # for each key, value in env_variable_group["values"]
+                        for key, value in env_variable_group["values"].items():
+                            # write key=value
+                            f.write("{}={}\n".format(key, value))
     
-    # for each file in microservices folder (except 'server')
-    # delete traditional .env file and create a new one with the same name
-    for folder in os.listdir("microservices"):
-        if folder == "server":
-            continue
-        
-        if os.path.exists("microservices/{}/.env".format(folder)):
-            os.remove("microservices/{}/.env".format(folder))
-        
-        # open utf-8 encoding
-        with open("microservices/{}/.env".format(folder), "w+", encoding="utf-8") as f:
-            # for each env_variable_group
-            for env_variable_group in env_variable_groups:
-                # if the folder is in the env_variable_group
-                if folder in env_variable_group["for"]:
-                    # for each key, value in env_variable_group["values"]
-                    for key, value in env_variable_group["values"].items():
-                        # write key=value
-                        f.write("{}={}\n".format(key, value))
+    def run(self):
+        self.prepare_docker_environment_variables()
+
             
 if __name__ == "__main__":
-    run()
+    setup_tool = DockerComposeSetupTool()
+    setup_tool.run()
