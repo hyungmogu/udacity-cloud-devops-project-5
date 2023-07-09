0b68460c7f19b2e117ac022698127d50e0c03f5e
Hyungmo Gu <guhyungm7@gmail.com>
2023-07-09 14:45:09 -0600

    feat: update progress
    - Fix error `CreateContainerConfigError` for `server-jpg`, `server-png`, `server-webp`
    - Fix error `image 'gateway' not found` error.

---
+++ microservices/gateway/docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# if .env file exists, export all variables from it
+if [ -f .env ]; then
+  export $(xargs < .env);
+fi
+
+# Remove docker image if it exists
+if [ "$(docker images -q "$GATEWAY_DOCKER_IMAGE_NAME:$GATEWAY_DOCKER_IMAGE_BUILD_NUMBER" 2> /dev/null)" != "" ]; then
+    docker image rm "$GATEWAY_DOCKER_IMAGE_NAME:$GATEWAY_DOCKER_IMAGE_BUILD_NUMBER";
+fi
+
+# Build docker image and push it to docker hub
+docker build -t "$GATEWAY_DOCKER_IMAGE_NAME:$GATEWAY_DOCKER_IMAGE_BUILD_NUMBER" . &&\ 
+docker login -u "$DOCKER_ID" -p "$DOCKER_PASSWORD" &&\
+docker tag "$GATEWAY_DOCKER_IMAGE_NAME:$GATEWAY_DOCKER_IMAGE_BUILD_NUMBER" "$DOCKER_ID/$GATEWAY_DOCKER_IMAGE_NAME:$GATEWAY_DOCKER_IMAGE_BUILD_NUMBER" &&\
+docker push "$DOCKER_ID/$GATEWAY_DOCKER_IMAGE_NAME:$GATEWAY_DOCKER_IMAGE_BUILD_NUMBER";
