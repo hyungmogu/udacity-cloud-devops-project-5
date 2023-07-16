7581028e0c7e4ba65076a4559975203a799d6857
Hyungmo Gu <guhyungm7@gmail.com>
2023-07-16 16:42:05 -0600

    feat: update progress
    - Add `Pillow` to the list of requirements for `gateway` component. The purpose is for testing.

--- microservices/gateway/tests/convert.py
+++ microservices/gateway/tests/convert.py
@@ -55,12 +55,32 @@ class TestSimplePositiveImgToPNG(unittest.TestCase):
 
                 # check if host is correct
                 host = urlparse(response.url).netloc
-                self.assertEqual(host, "{}:{}".format(SERVER_JPG_HOST, SERVER_JPG_PORT))
+                self.assertEqual(host, "{}:{}".format(SERVER_PNG_HOST, SERVER_PNG_PORT))
 
 # 2. check if sending a request to server-png contains correct header. It's okay if response is not 200.
 
 ## WEBP
 
 # 1. check if sending a request to server-webp has correct url. It's okay if response is not 200.
+class TestSimplePositiveImgToPNG(unittest.TestCase):
+    def setUp(self):
+        self.app = TestClient(app)
+
+    def test_convert_method_successfully_converts_a_valid_image_to_jpg(self):
+        with tempfile.NamedTemporaryFile(suffix=".jpg") as img_file:
+            img = Image.new("RGB", (50, 50), color="red")
+            img.save(img_file.name)
+
+            with open(img_file.name, "rb") as img_data:
+                response = self.app.post("/convert/to-webp",
+                                         files={"image": ("test.jpg", img_data, "image/jpg")})
+
+                # check response url pathname is correct
+                pathname = urlparse(response.url).path
+                self.assertEqual(pathname, "/convert/to-webp")
+
+                # check if host is correct
+                host = urlparse(response.url).netloc
+                self.assertEqual(host, "{}:{}".format(SERVER_WEBP_HOST, SERVER_WEBP_PORT))
 
 # 2. check if sending a request to server-webp contains correct header. It's okay if response is not 200.
