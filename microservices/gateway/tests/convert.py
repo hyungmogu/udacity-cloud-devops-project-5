a1e8672bc553abe87e53d03c769148638cc5127b
Hyungmo Gu <guhyungm7@gmail.com>
2023-07-16 10:59:40 -0600

    feat: update progress
    - Add pseudo code for `microservices/gateway/tests/convert.py`

---
+++ microservices/gateway/tests/convert.py
@@ -0,0 +1,17 @@
+## JPG
+
+# 1. check if sending a request to server-jpg has correct url. It's okay if response is not 200.
+
+# 2. check if sending a request to server-jpg contains correct header. It's okay if response is not 200.
+
+## PNG
+
+# 1. check if sending a request to server-png has correct url. It's okay if response is not 200.
+
+# 2. check if sending a request to server-png contains correct header. It's okay if response is not 200.
+
+## WEBP
+
+# 1. check if sending a request to server-webp has correct url. It's okay if response is not 200.
+
+# 2. check if sending a request to server-webp contains correct header. It's okay if response is not 200.
