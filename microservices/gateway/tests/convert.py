55e4dbaaf726d55be922c5163211e9ffafaa94b4
Hyungmo Gu <guhyungm7@gmail.com>
2023-07-16 15:56:46 -0600

    feat: update progress
    - Added a test about request to `/convert/to-png` for `gateway` microservices component

--- microservices/gateway/tests/convert.py
+++ microservices/gateway/tests/convert.py
@@ -36,6 +36,26 @@ class TestSimplePositiveImgToJPG(unittest.TestCase):
 ## PNG
 
 # 1. check if sending a request to server-png has correct url. It's okay if response is not 200.
+class TestSimplePositiveImgToPNG(unittest.TestCase):
+    def setUp(self):
+        self.app = TestClient(app)
+
+    def test_convert_method_successfully_converts_a_valid_image_to_jpg(self):
+        with tempfile.NamedTemporaryFile(suffix=".jpg") as img_file:
+            img = Image.new("RGB", (50, 50), color="red")
+            img.save(img_file.name)
+
+            with open(img_file.name, "rb") as img_data:
+                response = self.app.post("/convert/to-png",
+                                         files={"image": ("test.png", img_data, "image/jpg")})
+
+                # check response url pathname is correct
+                pathname = urlparse(response.url).path
+                self.assertEqual(pathname, "/convert/to-png")
+
+                # check if host is correct
+                host = urlparse(response.url).netloc
+                self.assertEqual(host, "{}:{}".format(SERVER_JPG_HOST, SERVER_JPG_PORT))
 
 # 2. check if sending a request to server-png contains correct header. It's okay if response is not 200.
 
