c26539a2c0a98ca1f4a380ef145b93243b62a606
Hyungmo Gu <guhyungm7@gmail.com>
2023-07-19 23:00:40 -0600

    feat: update progress
    - Removed sudo from prefixes in `docker_install_cicd.sh`. This is applied to all components in microservices.

--- microservices/server-png/docker_install_cicd.sh
+++ microservices/server-png/docker_install_cicd.sh
@@ -1,5 +1,5 @@
 apt-get update;
 apt-get install ca-certificates curl gnupg;
 install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
