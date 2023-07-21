3eabc3b1c34e2c7e59644ce89fe692be48ed692c
Hyungmo Gu <guhyungm7@gmail.com>
2023-07-21 08:15:20 -0600

    feat: update progress
    - Learned that buster docker image is based on debian, and not ubunutu. Updated `docker_install_cicd.sh`.
    `

--- microservices/gateway/docker_install_cicd.sh
+++ microservices/gateway/docker_install_cicd.sh
@@ -1,11 +1,14 @@
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do apt-get remove $pkg; done;
 apt-get update;
 apt-get install ca-certificates curl gnupg;
-install -m 0755 -d /etc/apt/keyrings;
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg;
-chmod a+r /etc/apt/keyrings/docker.gpg;
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
 echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null;
-sudo apt-get update;
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin;
+  tee /etc/apt/sources.list.d/docker.list > /dev/null;
+apt-get update;
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin;
+docker run hello-world;
+
