f1a9db6bf00439923fd6a6dadb8b6049c24b86f3
Hyungmo Gu <guhyungm7@gmail.com>
2023-07-21 21:57:43 -0600

    feat: update progress
    - Try another way of deploying image to docker. Followed this tutorial here: https://circleci.com/blog/how-to-build-a-docker-image-on-circleci-2-0/

--- microservices/gateway/docker_install_cicd.sh
+++ microservices/gateway/docker_install_cicd.sh
@@ -3,6 +3,6 @@ rm -r $HOME/.docker/desktop;
 rm /usr/local/bin/com.docker.cli;
 apt purge docker-desktop;
 apt-get update;
-apt-get install ./docker-desktop-4.21.1-x86_64.deb;
+apt-get install ./docker-desktop-<version>-<arch>.deb;
 systemctl --user start docker-desktop;
 docker run hello-world;
