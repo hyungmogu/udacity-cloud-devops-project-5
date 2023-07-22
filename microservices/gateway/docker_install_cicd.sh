aba119b945df2d43775865ce1601f37c18c5f1e7
Hyungmo Gu <guhyungm7@gmail.com>
2023-07-22 10:26:01 -0600

    feat: update progress
    - remove `docker_install_cicd.sh` from each microservices component. This is not needed as circleci base image comes with docker already.

--- microservices/gateway/docker_install_cicd.sh
+++
@@ -1,8 +0,0 @@
-apt remove docker-desktop;
-rm -r $HOME/.docker/desktop;
-rm /usr/local/bin/com.docker.cli;
-apt purge docker-desktop;
-apt-get update;
-apt-get install ./docker-desktop-<version>-<arch>.deb;
-systemctl --user start docker-desktop;
-docker run hello-world;
