6648532dfb06342ba8b8877f84e2f1d4b5039c76
Hyungmo Gu <guhyungm7@gmail.com>
2023-08-05 11:30:05 -0600

    feat: update progress
    - Add codes for performing microservices integration tests

---
+++ tests/integration_microservices/test_app.py
@@ -0,0 +1,11 @@
+import unittest
+
+# Check if gateway to server-jpg works correctly
+class TestSimplePositiveGatewayToServerJPG(unittest.TestCase):
+    pass
+# Check if gateway to server-png works correctly
+class TestSimplePositiveGatewayToServerPNG(unittest.TestCase):
+    pass
+# Check if gateway to server-webp works correctly
+class TestSimplePositiveGatewayToServerWEBP(unittest.TestCase):
+    pass
