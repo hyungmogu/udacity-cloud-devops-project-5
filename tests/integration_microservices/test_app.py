d2979d14e3048ffc8baebc2b393c6890c5086803
Hyungmo Gu <guhyungm7@gmail.com>
2023-08-05 12:01:59 -0600

    feat: update progress
    - Add test cases to microservices integration testing

--- tests/integration_microservices/test_app.py
+++ tests/integration_microservices/test_app.py
@@ -2,10 +2,19 @@ import unittest
 
 # Check if gateway to server-jpg works correctly
 class TestSimplePositiveGatewayToServerJPG(unittest.TestCase):
-    pass
+    def test_convert_method_successfully_converts_a_valid_image_to_jpg(self):
+        pass
+    def test_convert_method_returns_error_if_image_is_not_valid(self):
+        pass
 # Check if gateway to server-png works correctly
 class TestSimplePositiveGatewayToServerPNG(unittest.TestCase):
-    pass
+    def test_convert_method_successfully_converts_a_valid_image_to_png(self):
+        pass
+    def test_convert_method_returns_error_if_image_is_not_valid(self):
+        pass
 # Check if gateway to server-webp works correctly
 class TestSimplePositiveGatewayToServerWEBP(unittest.TestCase):
-    pass
+    def test_convert_method_successfully_converts_a_valid_image_to_webp(self):
+        pass
+    def test_convert_method_returns_error_if_image_is_not_valid(self):
+        pass
