993d17f3196f6c78380385caa4c792db3a833f2b
Hyungmo Gu <guhyungm7@gmail.com>
2023-08-05 12:46:06 -0600

    feat: update progress
    - Add setup codes for microservices integration tests

--- tests/integration_microservices/test_app.py
+++ tests/integration_microservices/test_app.py
@@ -1,19 +1,38 @@
+import os
 import unittest
+import boto3
+
+AWS_S3_BUCKET = "image-converter-test"
+os.environ["AWS_S3_BUCKET"] = AWS_S3_BUCKET
+os.environ["AWS_ACCESS_KEY_ID"] = "test"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "test"
 
 # Check if gateway to server-jpg works correctly
 class TestSimplePositiveGatewayToServerJPG(unittest.TestCase):
+    def setUp(self):
+        self.s3_resource = boto3.resource('s3')
+        self.s3_resource.create_bucket(Bucket=AWS_S3_BUCKET)        
+
     def test_convert_method_successfully_converts_a_valid_image_to_jpg(self):
         pass
     def test_convert_method_returns_error_if_image_is_not_valid(self):
         pass
 # Check if gateway to server-png works correctly
 class TestSimplePositiveGatewayToServerPNG(unittest.TestCase):
+    def setUp(self):
+        self.s3_resource = boto3.resource('s3')
+        self.s3_resource.create_bucket(Bucket=AWS_S3_BUCKET)        
+
     def test_convert_method_successfully_converts_a_valid_image_to_png(self):
         pass
     def test_convert_method_returns_error_if_image_is_not_valid(self):
         pass
 # Check if gateway to server-webp works correctly
 class TestSimplePositiveGatewayToServerWEBP(unittest.TestCase):
+    def setUp(self):
+        self.s3_resource = boto3.resource('s3')
+        self.s3_resource.create_bucket(Bucket=AWS_S3_BUCKET)        
+
     def test_convert_method_successfully_converts_a_valid_image_to_webp(self):
         pass
     def test_convert_method_returns_error_if_image_is_not_valid(self):
