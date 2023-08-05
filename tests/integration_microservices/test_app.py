d796364f90ccdfd0e9528a4ede904d736d1f11b9
Hyungmo Gu <guhyungm7@gmail.com>
2023-08-05 13:25:20 -0600

    feat: update progress
    - Add attribute `minikube_service_url` to `tests/integration_microservices/test_app.py` for microservice integration testing. This will be used to make connection requests

--- tests/integration_microservices/test_app.py
+++ tests/integration_microservices/test_app.py
@@ -1,6 +1,11 @@
+import sys
 import os
 import unittest
+import requests
+from dotenv import find_dotenv, load_dotenv
 import boto3
+
+load_dotenv(find_dotenv())
 
 AWS_S3_BUCKET = "image-converter-test"
 os.environ["AWS_S3_BUCKET"] = AWS_S3_BUCKET
@@ -11,17 +16,25 @@ os.environ["AWS_SECRET_ACCESS_KEY"] = "test"
 class TestSimplePositiveGatewayToServerJPG(unittest.TestCase):
     def setUp(self):
         self.s3_resource = boto3.resource('s3')
-        self.s3_resource.create_bucket(Bucket=AWS_S3_BUCKET)        
+        self.s3_resource.create_bucket(Bucket=AWS_S3_BUCKET)
+        self.minikube_service_url = os.environ.get('MINIKUBE_SERVICE_URL', "")
 
+        if len(self.minikube_service_url) == 0:
+            raise Exception("MINIKUBE_SERVICE_URL environment variable is not set")
+        
     def test_convert_method_successfully_converts_a_valid_image_to_jpg(self):
-        pass
+        pass        
     def test_convert_method_returns_error_if_image_is_not_valid(self):
         pass
 # Check if gateway to server-png works correctly
 class TestSimplePositiveGatewayToServerPNG(unittest.TestCase):
     def setUp(self):
         self.s3_resource = boto3.resource('s3')
-        self.s3_resource.create_bucket(Bucket=AWS_S3_BUCKET)        
+        self.s3_resource.create_bucket(Bucket=AWS_S3_BUCKET)
+        self.minikube_service_url = os.environ.get('MINIKUBE_SERVICE_URL', "")
+
+        if len(self.minikube_service_url) == 0:
+            raise Exception("MINIKUBE_SERVICE_URL environment variable is not set")
 
     def test_convert_method_successfully_converts_a_valid_image_to_png(self):
         pass
@@ -31,8 +44,12 @@ class TestSimplePositiveGatewayToServerPNG(unittest.TestCase):
 class TestSimplePositiveGatewayToServerWEBP(unittest.TestCase):
     def setUp(self):
         self.s3_resource = boto3.resource('s3')
-        self.s3_resource.create_bucket(Bucket=AWS_S3_BUCKET)        
+        self.s3_resource.create_bucket(Bucket=AWS_S3_BUCKET)
+        self.minikube_service_url = os.environ.get('MINIKUBE_SERVICE_URL', "")
 
+        if len(self.minikube_service_url) == 0:
+            raise Exception("MINIKUBE_SERVICE_URL environment variable is not set")
+        
     def test_convert_method_successfully_converts_a_valid_image_to_webp(self):
         pass
     def test_convert_method_returns_error_if_image_is_not_valid(self):
