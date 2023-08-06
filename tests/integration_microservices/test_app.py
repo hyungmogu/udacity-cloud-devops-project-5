61107a805bacc5d807cfa3efd57c0bcb746b70be
Hyungmo Gu <guhyungm7@gmail.com>
2023-08-06 08:46:14 -0600

    feat: update progress
    - define test methods `test_convert_method_successfully_converts_a_valid_image` for microservice integration tests

--- tests/integration_microservices/test_app.py
+++ tests/integration_microservices/test_app.py
@@ -2,6 +2,8 @@ import sys
 import os
 import unittest
 import requests
+import tempfile
+from PIL import Image
 from dotenv import find_dotenv, load_dotenv
 import boto3
 
@@ -18,12 +20,24 @@ class TestSimplePositiveGatewayToServerJPG(unittest.TestCase):
         self.s3_resource = boto3.resource('s3')
         self.s3_resource.create_bucket(Bucket=AWS_S3_BUCKET)
         self.minikube_service_url = os.environ.get('MINIKUBE_SERVICE_URL', "")
+        self.gateway_port = os.environ.get("GATEWAY_PORT", "")
 
         if len(self.minikube_service_url) == 0:
             raise Exception("MINIKUBE_SERVICE_URL environment variable is not set")
         
     def test_convert_method_successfully_converts_a_valid_image_to_jpg(self):
-        pass        
+        with tempfile.NamedTemporaryFile(suffix=".png") as img_file:
+            img = Image.new("RGB", (50, 50), color="red")
+            img.save(img_file.name)
+
+            with open(img_file.name, "rb") as img_data:
+                # use request to send the image to the server-jpg path /convert/to-jpg via port 8010
+                response = requests.post(
+                    f"{self.minikube_service_url}:{self.gateway_port}/convert/to-jpg", 
+                    files={"image": ("test.png", img_data, "image/png")}, timeout=10)
+
+                self.assertEqual(response.status_code, 201)
+
     def test_convert_method_returns_error_if_image_is_not_valid(self):
         pass
 # Check if gateway to server-png works correctly
@@ -32,12 +46,24 @@ class TestSimplePositiveGatewayToServerPNG(unittest.TestCase):
         self.s3_resource = boto3.resource('s3')
         self.s3_resource.create_bucket(Bucket=AWS_S3_BUCKET)
         self.minikube_service_url = os.environ.get('MINIKUBE_SERVICE_URL', "")
+        self.gateway_port = os.environ.get("GATEWAY_PORT", "")
 
         if len(self.minikube_service_url) == 0:
             raise Exception("MINIKUBE_SERVICE_URL environment variable is not set")
 
     def test_convert_method_successfully_converts_a_valid_image_to_png(self):
-        pass
+        with tempfile.NamedTemporaryFile(suffix=".jpg") as img_file:
+            img = Image.new("RGB", (50, 50), color="red")
+            img.save(img_file.name)
+
+            with open(img_file.name, "rb") as img_data:
+                # use request to send the image to the server-png path /convert/to-png via port 8010
+                response = requests.post(
+                    f"{self.minikube_service_url}:{self.gateway_port}/convert/to-png", 
+                    files={"image": ("test.jpg", img_data, "image/jpg")}, timeout=10)
+
+                self.assertEqual(response.status_code, 201)
+
     def test_convert_method_returns_error_if_image_is_not_valid(self):
         pass
 # Check if gateway to server-webp works correctly
@@ -46,11 +72,23 @@ class TestSimplePositiveGatewayToServerWEBP(unittest.TestCase):
         self.s3_resource = boto3.resource('s3')
         self.s3_resource.create_bucket(Bucket=AWS_S3_BUCKET)
         self.minikube_service_url = os.environ.get('MINIKUBE_SERVICE_URL', "")
+        self.gateway_port = os.environ.get("GATEWAY_PORT", "")
 
         if len(self.minikube_service_url) == 0:
             raise Exception("MINIKUBE_SERVICE_URL environment variable is not set")
         
     def test_convert_method_successfully_converts_a_valid_image_to_webp(self):
-        pass
+        with tempfile.NamedTemporaryFile(suffix=".jpg") as img_file:
+            img = Image.new("RGB", (50, 50), color="red")
+            img.save(img_file.name)
+
+            with open(img_file.name, "rb") as img_data:
+                # use request to send the image to the server-webp path /convert/to-webp via port 8010
+                response = requests.post(
+                    f"{self.minikube_service_url}:{self.gateway_port}/convert/to-webp", 
+                    files={"image": ("test.jpg", img_data, "image/jpg")}, timeout=10)
+
+                self.assertEqual(response.status_code, 201)
+
     def test_convert_method_returns_error_if_image_is_not_valid(self):
         pass
