7dc8ea3980cce514217802fad13a9fc3b60f2661
Hyungmo Gu <guhyungm7@gmail.com>
2023-08-07 14:19:43 -0600

    feat: update progress
    - Remove gateway port from `tests/integration_microservices/test_app.py`.

--- tests/integration_microservices/test_app.py
+++ tests/integration_microservices/test_app.py
@@ -19,13 +19,12 @@ class TestSimplePositiveGatewayToServerJPG(unittest.TestCase):
         self.s3_resource = boto3.resource('s3')
         self.s3_resource.create_bucket(Bucket=AWS_S3_BUCKET)
         self.minikube_service_url = os.environ.get('MINIKUBE_SERVICE_URL', "")
-        self.gateway_port = os.environ.get("GATEWAY_PORT", "")
 
         if len(self.minikube_service_url) == 0:
             raise Exception("MINIKUBE_SERVICE_URL environment variable is not set")
 
     def test_health_endpoint_successfully_returns_ok(self):
-        response = requests.get(f"{self.minikube_service_url}:{self.gateway_port}/health", timeout=10)
+        response = requests.get(f"{self.minikube_service_url}/health", timeout=10)
         self.assertEqual(response.status_code, 200)
     
     def test_convert_method_successfully_converts_a_valid_image_to_jpg(self):
@@ -35,7 +34,7 @@ class TestSimplePositiveGatewayToServerJPG(unittest.TestCase):
 
             with open(img_file.name, "rb") as img_data:
                 response = requests.post(
-                    f"{self.minikube_service_url}:{self.gateway_port}/convert/to-jpg", 
+                    f"{self.minikube_service_url}/convert/to-jpg", 
                     files={"image": ("test.png", img_data, "image/png")}, timeout=10)
 
                 self.assertEqual(response.status_code, 201)
@@ -46,7 +45,7 @@ class TestSimplePositiveGatewayToServerJPG(unittest.TestCase):
 
             with open(txt_file.name, "rb") as txt_data:
                 response = requests.post(
-                    f"{self.minikube_service_url}:{self.gateway_port}/convert/to-jpg",
+                    f"{self.minikube_service_url}/convert/to-jpg",
                     files={"image": ("test.txt", txt_data, "text/plain")}, timeout=10)
 
                 self.assertEqual(response.status_code, 415)
@@ -57,13 +56,12 @@ class TestSimplePositiveGatewayToServerPNG(unittest.TestCase):
         self.s3_resource = boto3.resource('s3')
         self.s3_resource.create_bucket(Bucket=AWS_S3_BUCKET)
         self.minikube_service_url = os.environ.get('MINIKUBE_SERVICE_URL', "")
-        self.gateway_port = os.environ.get("GATEWAY_PORT", "")
-
+        
         if len(self.minikube_service_url) == 0:
             raise Exception("MINIKUBE_SERVICE_URL environment variable is not set")
     
     def test_health_endpoint_successfully_returns_ok(self):
-        response = requests.get(f"{self.minikube_service_url}:{self.gateway_port}/health", timeout=10)
+        response = requests.get(f"{self.minikube_service_url}/health", timeout=10)
         self.assertEqual(response.status_code, 200)
 
     def test_convert_method_successfully_converts_a_valid_image_to_png(self):
@@ -73,7 +71,7 @@ class TestSimplePositiveGatewayToServerPNG(unittest.TestCase):
 
             with open(img_file.name, "rb") as img_data:
                 response = requests.post(
-                    f"{self.minikube_service_url}:{self.gateway_port}/convert/to-png", 
+                    f"{self.minikube_service_url}/convert/to-png", 
                     files={"image": ("test.jpg", img_data, "image/jpg")}, timeout=10)
 
                 self.assertEqual(response.status_code, 201)
@@ -84,7 +82,7 @@ class TestSimplePositiveGatewayToServerPNG(unittest.TestCase):
 
             with open(txt_file.name, "rb") as txt_data:
                 response = requests.post(
-                    f"{self.minikube_service_url}:{self.gateway_port}/convert/to-png",
+                    f"{self.minikube_service_url}/convert/to-png",
                     files={"image": ("test.txt", txt_data, "text/plain")}, timeout=10)
 
                 self.assertEqual(response.status_code, 415)
@@ -95,13 +93,12 @@ class TestSimplePositiveGatewayToServerWEBP(unittest.TestCase):
         self.s3_resource = boto3.resource('s3')
         self.s3_resource.create_bucket(Bucket=AWS_S3_BUCKET)
         self.minikube_service_url = os.environ.get('MINIKUBE_SERVICE_URL', "")
-        self.gateway_port = os.environ.get("GATEWAY_PORT", "")
 
         if len(self.minikube_service_url) == 0:
             raise Exception("MINIKUBE_SERVICE_URL environment variable is not set")
     
     def test_health_endpoint_successfully_returns_ok(self):
-        response = requests.get(f"{self.minikube_service_url}:{self.gateway_port}/health", timeout=10)
+        response = requests.get(f"{self.minikube_service_url}/health", timeout=10)
         self.assertEqual(response.status_code, 200)
         
     def test_convert_method_successfully_converts_a_valid_image_to_webp(self):
@@ -111,7 +108,7 @@ class TestSimplePositiveGatewayToServerWEBP(unittest.TestCase):
 
             with open(img_file.name, "rb") as img_data:
                 response = requests.post(
-                    f"{self.minikube_service_url}:{self.gateway_port}/convert/to-webp", 
+                    f"{self.minikube_service_url}/convert/to-webp", 
                     files={"image": ("test.jpg", img_data, "image/jpg")}, timeout=10)
 
                 self.assertEqual(response.status_code, 201)
@@ -122,7 +119,7 @@ class TestSimplePositiveGatewayToServerWEBP(unittest.TestCase):
 
             with open(txt_file.name, "rb") as txt_data:
                 response = requests.post(
-                    f"{self.minikube_service_url}:{self.gateway_port}/convert/to-webp",
+                    f"{self.minikube_service_url}/convert/to-webp",
                     files={"image": ("test.txt", txt_data, "text/plain")}, timeout=10)
 
                 self.assertEqual(response.status_code, 415)
