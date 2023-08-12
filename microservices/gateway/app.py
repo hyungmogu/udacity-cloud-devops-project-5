84f9943b6b27a368b58cc6345c10661eb6194e1b
Hyungmo Gu <guhyungm7@gmail.com>
2023-08-11 23:15:56 -0600

    feat: update progress
    - constructing `startup_redis` function inside `gateway/app.py` to see if it resolves `redis.exceptions.ResponseError: MOVED` error

--- microservices/gateway/app.py
+++ microservices/gateway/app.py
@@ -13,15 +13,8 @@ from config import REDIS_HOST, REDIS_PASSWORD, GATEWAY_PORT
 
 logging.basicConfig(level=logging.DEBUG)
 
-app = FastAPI()
-
-app.include_router(convert_router, prefix="/convert", tags=["Convert"])
-app.include_router(health_router, prefix="/health", tags=["Health"])
-
-@app.on_event("startup")
-async def startup_event():
+async def startup_redis():
     logging.debug("hello")
-    httpx_client_wrapper.start()
     REDIS_PASSWORD_ECODE_SAFE = quote(REDIS_PASSWORD, safe="")
     try:
         redis_c = redis_async.from_url("redis://{}@{}:6379".format(REDIS_PASSWORD_ECODE_SAFE, REDIS_HOST), encoding="utf-8", decode_responses=True)
@@ -47,10 +40,20 @@ async def startup_event():
             # Handle other types of Redis errors
             raise
 
+app = FastAPI()
+
+startup_redis()
+
+@app.on_event("startup")
+async def startup_event():
+    httpx_client_wrapper.start()
+
 @app.on_event("shutdown")
 async def shutdown_event():
     await httpx_client_wrapper.stop()
 
+app.include_router(convert_router, prefix="/convert", tags=["Convert"])
+app.include_router(health_router, prefix="/health", tags=["Health"])
 
 if __name__ == '__main__':
     uvicorn.run("app:app", host="0.0.0.0", port=GATEWAY_PORT, reload=True) 
