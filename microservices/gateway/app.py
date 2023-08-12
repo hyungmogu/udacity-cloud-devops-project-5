02ff3c3151a1e9e575f88f639b3b489baf3795cc
Hyungmo Gu <guhyungm7@gmail.com>
2023-08-11 23:01:52 -0600

    feat: update progress
    - Add hello statement to check if `startup` event inside `gateway` microservice component would active when server runs

--- microservices/gateway/app.py
+++ microservices/gateway/app.py
@@ -20,10 +20,15 @@ app.include_router(health_router, prefix="/health", tags=["Health"])
 
 @app.on_event("startup")
 async def startup_event():
+    logging.debug("hello")
     httpx_client_wrapper.start()
     REDIS_PASSWORD_ECODE_SAFE = quote(REDIS_PASSWORD, safe="")
     try:
         redis_c = redis_async.from_url("redis://{}@{}:6379".format(REDIS_PASSWORD_ECODE_SAFE, REDIS_HOST), encoding="utf-8", decode_responses=True)
+        async with redis_c.pipeline(transaction=True) as pipe:
+            ok1, ok2 = await (pipe.set("key1", "value1").set("key2", "value2").execute())
+        assert ok1
+        assert ok2
         await FastAPILimiter.init(redis_c)
     except redis.exceptions.ResponseError as error:
         if error.args[0].startswith('MOVED'):
@@ -33,6 +38,10 @@ async def startup_event():
 
             # Update the Redis client connection to the new node
             redis_c = redis_async.from_url("redis://{}@{}:{}".format(REDIS_PASSWORD_ECODE_SAFE, new_host, new_port), encoding="utf-8", decode_responses=True)
+            async with redis_c.pipeline(transaction=True) as pipe:
+                ok1, ok2 = await (pipe.set("key1", "value1").set("key2", "value2").execute())
+            assert ok1
+            assert ok2
             await FastAPILimiter.init(redis_c)
         else:
             # Handle other types of Redis errors
