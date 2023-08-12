0bf0222b5621fb5aab0ff5a9ba50f58510db26b0
Hyungmo Gu <guhyungm7@gmail.com>
2023-08-11 22:17:21 -0600

    feat: update progress
    - Add code in attempt to resolve `redis.exceptions.responseerror moved` error

--- microservices/gateway/app.py
+++ microservices/gateway/app.py
@@ -1,6 +1,7 @@
 import uvicorn
 import logging
-import redis.asyncio as redis
+import redis
+import redis.asyncio as redis_async
 from urllib.parse import quote
 from fastapi import FastAPI
 from fastapi_limiter import FastAPILimiter
@@ -21,8 +22,21 @@ app.include_router(health_router, prefix="/health", tags=["Health"])
 async def startup_event():
     httpx_client_wrapper.start()
     REDIS_PASSWORD_ECODE_SAFE = quote(REDIS_PASSWORD, safe="")
-    redis_c = redis.from_url("redis://{}@{}:6379".format(REDIS_PASSWORD_ECODE_SAFE, REDIS_HOST), encoding="utf-8", decode_responses=True)
-    await FastAPILimiter.init(redis_c)
+    try:
+        redis_c = redis_async.from_url("redis://{}@{}:6379".format(REDIS_PASSWORD_ECODE_SAFE, REDIS_HOST), encoding="utf-8", decode_responses=True)
+        await FastAPILimiter.init(redis_c)
+    except redis.exceptions.ResponseError as error:
+        if error.args[0].startswith('MOVED'):
+            # Extract the new node information from the error response
+            moved_info = error.args[0].split(' ')
+            new_host, new_port = moved_info[2].split(':')
+
+            # Update the Redis client connection to the new node
+            redis_c = redis_async.from_url("redis://{}@{}:{}".format(REDIS_PASSWORD_ECODE_SAFE, new_host, new_port), encoding="utf-8", decode_responses=True)
+            await FastAPILimiter.init(redis_c)
+        else:
+            # Handle other types of Redis errors
+            raise
 
 @app.on_event("shutdown")
 async def shutdown_event():
