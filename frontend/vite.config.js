8aa4f4103bf406bbf4ced0c3143b54adf87da133
Hyungmo Gu <guhyungm7@gmail.com>
2023-08-20 21:36:51 -0600

    feat: update progress
    - Resolve front end test failure in `.circleci/config.yml`. This is caused by a series of undocumented changes required to make the app to accept .env file, and maintain harmonious relationship with the test program. In sum, this is a configuration error. After this update, I hope the CICD would travel upto or beyond `smoke-test` section.
    
    The goal of this action is to resolve `test-frontend` section failure in `.circleci/config.yml`.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,31 @@
-import { fileURLToPath, URL } from 'node:url'
+import {
+  fileURLToPath,
+  URL
+} from 'node:url'
 
-import { defineConfig, loadEnv } from 'vite';
+import {
+  defineConfig,
+  loadEnv
+} from 'vite';
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-// import CONFIG from './config.js'
 
 // https://vitejs.dev/config/
-export default ({mode}) => {
-  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
-  return defineConfig({
+export default defineConfig(({
+  mode
+}) => {
+  process.env = {
+    ...process.env,
+    ...loadEnv(mode, process.cwd())
+  };
+  return {
     base: process.env.VITE_IS_PRODUCTION === "true" ? "/" : "/udacity-cloud-devops-project-5/preview/",
     plugins: [vue(), vueJsx()],
     resolve: {
       alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
+        '@': fileURLToPath(new URL('./src',
+          import.meta.url))
       }
     }
-  })
-}
+  }
+});
